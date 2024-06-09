a13592851ed4ace1c59bbeb5b1f56b5b40541caa
Daniel <nifemiolaniyi4@gmail.com>
2024-06-09 09:38:53 +0100

    ---

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const word = [];
-const origin = [];
-const asked = [];
+let word = [];
+let origin = [];
+let asked = [];
 let score = 0;
 let currentTxt = '';
 let ans = '';
 let no = 0;
 let chance = 0;
+let letterNo = 1;
 let correct = false;
 const WORDS = document.getElementById('words').innerText.split(',');
 const alpha = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(',');
-async function init(str){
+async function init(){
+    if(localStorage.hasOwnProperty(alpha[letterNo])){
+        document.getElementById('save').innerText = 'Continue';
+    }
     try {
-        const data = WORDS[alpha.indexOf(str)];
+        const data = WORDS[letterNo - 1];
+        console.log(data)
         const wordAll = data.split(' ');
         for (let i = 0; i < wordAll.length; i++) {
-            if(i === 1 || isEven(i)){ 
+            if(i === 0 || isEven(i)){
                 word.push(wordAll[i]);
             } else {
                 origin.push(wordAll[i]);
             }
         }
-        if(word.includes('') || origin.includes('')){
+        console.log(word)
+        if(word.includes(' ') || word.includes('')){
             console.log('Error at'+word[word.indexOf(' ')]+' before'+word[word.indexOf(' ')]-1);
         }
     } catch (error) {
@@ -43,7 +49,7 @@ function speak(str){
     speech.speak(utterance);
 };
 function getRandom(max){
-    const no = Math.floor((Math.random() * max));
+    const no = Math.floor(Math.random() * max);
     return no;
 }
 function available(){
@@ -57,24 +63,39 @@ function available(){
         arrayObj.word.push(word[i]);
         arrayObj.origin.push(origin[i]);
     }
+    if(arrayObj.word.length === 0){
+        arrayObj.word.push('null');
+    }
     return arrayObj
 }
 function generateNewWord(){
     const {word, origin} = available();
-    const no = getRandom(word.length-1);
+    const no = getRandom(word.length);
 
-    const speech ={
-        text: word[no],
-        org: origin[no]
+    if(word[0] !== 'null'){
+        const speech ={
+            text: word[no],
+            org: origin[no]
+        }
+        asked.push(word[no]);
+        ans = speech.text;
+        
+
+        return speech
+    } else {
+        return {
+            text: null,
+            org: null
+        }
     }
-    asked.push(word[no]);
-    ans = speech.text;
-
-    return speech
 }
 function callWord(){
     const {text, org} = generateNewWord();
-    currentTxt = 'Spell the word... '+text+' ...Origin of '+org;
+    if(text){
+        currentTxt = 'Spell the word... '+text+' ...Origin of '+org;
+    } else {
+        currentTxt = 'You have successfully finished the letter '+alpha[letterNo - 1];
+    }
     speak(currentTxt);
     no = 0;
 }
@@ -94,13 +115,13 @@ function assessInput(){
         speak('Correct');
         correct = true
         input.style.border = '2px solid goldenrod';
-        input.innerText = '';
+        input.value = '';
     } else {
         score -= 10;
         speak('Wrong');
         correct = false;
         input.style.border = '2px solid red';
-        input.innerText = '';
+        input.value = '';
     }
     displayScore()
 }
@@ -110,9 +131,58 @@ function displayScore(){
 }
 function displayAns(){
     const output = document.getElementById('output');
-    const input = document.getElementById('input');
-    input.innerText = '';
     output.innerText = ans.toUpperCase();
+}
+function resetTexts(){
+    ans = '';
+    document.getElementById('start').innerText = 'Start';
+    document.getElementById('input').innerText = '';
+    document.getElementById('alphabet').innerText = alpha[letterNo - 1];
+}
+function saveData(){
+    const json = JSON.stringify({
+        word: word,
+        origin: origin,
+        asked: asked,
+        no: no,
+        currentTxt: currentTxt,
+        correct: correct,
+        chance: chance,
+        score: score,
+        ans: ans
+    });
+
+    localStorage.setItem(alpha[letterNo - 1], json);
+}
+function getData(){
+    const response = localStorage.getItem(alpha[letterNo - 1]);
+
+    const data = JSON.parse(response);
+
+    word = data.word;
+    origin = data.origin;
+    asked = data.asked;
+    no = data.no;
+    currentTxt = data.currentTxt;
+    correct = data.correct;
+    chance = data.chance;
+    score = data.score;
+    ans = data.ans;
+    document.getElementById('save').innerText = 'Save';
+}
+function clearData(){
+    localStorage.removeItem(alpha[letterNo - 1]);
+    word = [];
+    origin = [];
+    asked = [];
+    no = 0;
+    currentTxt = '';
+    correct = false;
+    chance = 0;
+    score = 0;
+    ans = '';
+    resetTexts();
+    document.getElementById('save').innerText = 'Save';
 }
 document.getElementById('start').addEventListener('click', () => {
     switch(document.getElementById('start').innerText){
@@ -126,7 +196,8 @@ document.getElementById('start').addEventListener('click', () => {
     }
 });
 document.getElementById('send').addEventListener('click', () => {
-    if(chance < 3 && !correct){
+    chance++
+    if(chance <= 3 && !correct){
         assessInput();
         if(correct){
             correct = false;
@@ -134,11 +205,46 @@ document.getElementById('send').addEventListener('click', () => {
             callWord();
             chance = 0;
         }
-        chance++
     } else {
         displayAns();
         callWord();
         chance = 0
     }
 });
-init('A');
+document.getElementById('forward').addEventListener('click', () => {
+    if(letterNo >= 26){
+        letterNo = 1;
+    } else {
+        letterNo += 1;
+    }
+
+    word = [];
+    origin = [];
+    init();
+    resetTexts();
+});
+document.getElementById('backward').addEventListener('click', () => {
+    if(letterNo <- 0){
+        letterNo = 1;
+    } else {
+        letterNo -= 1;
+    }
+
+    init();
+    resetTexts();
+});
+document.getElementById('save').addEventListener('click', () => {
+    if(localStorage.hasOwnProperty(alpha[letterNo - 1])){
+        document.getElementById('save').innerText = 'Save';
+        document.getElementById('start').innerText = 'Start';
+        getData();
+    } else {
+        saveData();
+    }
+});
+document.getElementById('reset').addEventListener('click', () => {
+    clearData();
+    init();
+});
+
+init();
